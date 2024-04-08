d8c6a7cd9e27b84fec8968f8b74850601e15202d
haeram27 <haeram27@gmail.com>
2024-04-08 22:23:34 +0900

    .

--- java/src/test/java/com/example/sample/quiz/bruteforce/FindingPrimeTests.java
+++ java/src/test/java/com/example/sample/quiz/bruteforce/FindingPrimeTests.java
@@ -12,25 +12,39 @@ public class FindingPrimeTests {
      * find all primes using every character of given string
      */
 
+    /*
+     * Quest
+     */
     @ParameterizedTest
     @ValueSource(strings = {"17", "011"}) // 3, 2
     @Timeout(value = 3, threadMode = Timeout.ThreadMode.SEPARATE_THREAD)
     public void solution(String numbers) {
-        HashSet<Integer> set = new HashSet<>();
-        permutation("", numbers, set);
+        HashSet<Integer> primes = new HashSet<>();
+        // TODO:
 
-        int count = 0;
-        for (var a : set) {
-            // System.out.println(a);
-
-            if (isPrime(a))
-                count++;
-        }
-
-        System.err.println(count);
+        System.err.println(primes.size());
     }
 
-    boolean isPrime(int x) {
+
+    /*
+     * Answer
+     */
+    @ParameterizedTest
+    @ValueSource(strings = {"17", "011"}) // 3, 2
+    @Timeout(value = 3, threadMode = Timeout.ThreadMode.SEPARATE_THREAD)
+    public void solutionA(String numbers) {
+        HashSet<Integer> primes = new HashSet<>();
+        char[] data = numbers.toCharArray();
+        for (int i = 1; i <= numbers.length(); i++) {
+            char[] out = new char[i];
+            boolean[] visited = new boolean[numbers.length()];
+            permA(data, out, i, 0, visited, primes);
+        }
+
+        System.err.println(primes.size());
+    }
+
+    boolean isPrimeA(int x) {
         if (x <= 1)
             return false;
         else if (x == 2)
@@ -47,16 +61,21 @@ public class FindingPrimeTests {
         return true;
     }
 
-    public void permutation(String prefix, String str, HashSet<Integer> set) {
-        int n = str.length();
-        // if (n == 0)
-        //     System.out.println(prefix);
-
-        if (!prefix.equals(""))
-            set.add(Integer.valueOf(prefix));
-
-        for (int i = 0; i < n; i++)
-            permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i + 1, n), set);
+    void permA(char[] data, char[] out, int r, int depth, boolean[] visited, HashSet<Integer> primes) {
+        if (depth == r) {
+            int x = Integer.valueOf(new String(out));
+            if (isPrimeA(x))
+                primes.add(x);
+            return;
+        }
+        for (int i = 0; i < data.length; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                out[depth] = data[i];
+                permA(data, out, r, depth + 1, visited, primes);
+                visited[i] = false;
+            }
+        }
     }
 
 }
