f42ce28f0d22b3985287f50e27cb0159719d0ea2
haeram27 <haeram27@gmail.com>
2024-04-08 12:22:39 +0900

    update lent shirts test

--- java/src/test/java/com/example/sample/quiz/greedy/LentShirtsTests.java
+++ java/src/test/java/com/example/sample/quiz/greedy/LentShirtsTests.java
@@ -1,12 +1,10 @@
 package com.example.sample.quiz.greedy;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 // import java.util.*;
 // import java.util.stream.*;
 import java.util.Arrays;
 import java.util.stream.Stream;
-
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -26,34 +24,52 @@ public class LentShirtsTests {
 
     @ParameterizedTest
     @MethodSource("argSupplier")
-    public void solutionA(int n, int[] lost, int[] reserve, int correctanswer) {
-        int[] people = new int[n];
+    public void quiz(int n, int[] lost, int[] reserve, int expected) {
         int answer = n;
+        //TODO:
 
-        for (int l : lost)
-            people[l - 1]--;
-        for (int r : reserve)
-            people[r - 1]++;
-
-        for (int i = 0; i < people.length; i++) {
-            if (people[i] == -1) {
-                if (i - 1 >= 0 && people[i - 1] == 1) {
-                    people[i]++;
-                    people[i - 1]--;
-                } else if (i + 1 < people.length && people[i + 1] == 1) {
-                    people[i]++;
-                    people[i + 1]--;
-                } else
-                    answer--;
-            }
-        }
-
-        assertEquals(correctanswer, answer);
+        System.out.println(expected + " " + answer);
+        assertEquals(expected, answer);
     }
 
     @ParameterizedTest
     @MethodSource("argSupplier")
-    public void solutionB(int n, int[] lost, int[] reserve, int correctanswer) {
+    public void solutionA(int n, int[] lost, int[] reserve, int expected) {
+        int answer = n;
+        int[] people = new int[n + 1];
+
+        for (int i : lost) {
+            people[i]--;
+        }
+
+        for (int i : reserve) {
+            people[i]++;
+        }
+
+        for (int i = 1; i < people.length; i++) {
+            if (people[i] < 0)
+                answer--;
+        }
+
+        for (int i = 1; i < people.length; i++) {
+            if (people[i] < 0 && i - 1 > 0 && people[i - 1] > 0) {
+                people[i - 1]--;
+                people[i]++;
+                answer++;
+            } else if (people[i] < 0 && i + 1 < people.length && people[i + 1] > 0) {
+                people[i + 1]--;
+                people[i]++;
+                answer++;
+            }
+        }
+        System.out.println(Arrays.toString(people));
+        System.out.println(expected + " " + answer);
+        assertEquals(expected, answer);
+    }
+
+    @ParameterizedTest
+    @MethodSource("argSupplier")
+    public void solutionB(int n, int[] lost, int[] reserve, int expected) {
         int answer = 0;
         int llen = lost.length;
         int rlen = reserve.length;
@@ -82,6 +98,6 @@ public class LentShirtsTests {
             }
         }
 
-        assertEquals(correctanswer, answer);
+        assertEquals(expected, answer);
     }
 }
