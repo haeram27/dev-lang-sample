d923538a9f07280b6d2c46aa3e22156ab6fa5a65
haeram27 <haeram27@gmail.com>
2024-05-30 20:46:56 +0900

    add jackson test

--- java/src/test/java/com/example/sample/exam/Base64Tests.java
+++ java/src/test/java/com/example/sample/exam/Base64Tests.java
@@ -1,14 +1,15 @@
 package com.example.sample.exam;
 
 import java.util.Base64;
+
 import org.junit.jupiter.api.Test;
 
 public class Base64Tests {
 
     @Test
     void base64Test() {
-        byte[] originalBytes = new byte[] {(byte) 255, (byte) 128, (byte) 64, (byte) 32, (byte) 16, (byte) 8, (byte) 4,
-                (byte) 2, (byte) 1};
+        byte[] originalBytes = new byte[] { (byte) 255, (byte) 128, (byte) 64, (byte) 32, (byte) 16, (byte) 8, (byte) 4,
+                (byte) 2, (byte) 1 };
 
         for (var b : originalBytes) {
             System.out.println(Integer.toBinaryString(Byte.toUnsignedInt(b)));
