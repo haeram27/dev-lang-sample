07d3dd68d4cd5854767ce43e189c212621f6bc8d
haeram27 <haeram27@gmail.com>
2024-06-11 14:53:25 +0900

    add base64 string test

--- java/src/test/java/com/example/sample/exam/Base64Tests.java
+++ java/src/test/java/com/example/sample/exam/Base64Tests.java
@@ -1,5 +1,6 @@
 package com.example.sample.exam;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 import org.junit.jupiter.api.Test;
@@ -18,5 +19,18 @@ public class Base64Tests {
         String base64Encoded = Base64.getEncoder().encodeToString(originalBytes);
         System.out.println(base64Encoded);
         byte[] base64Decoded = Base64.getDecoder().decode(base64Encoded);
+    }
+
+    @Test
+    void base64StringEncodeTest() {
+        String plain = "xxxxxx<iframe src=\"javascript:alert(parent.document.domain)\">22222";
+        String encoded = Base64.getEncoder().encodeToString(plain.getBytes(StandardCharsets.UTF_8));
+        System.out.println("## decoded:----");
+        System.out.println(encoded);
+
+        String decoded = new String(Base64.getDecoder().decode(encoded.getBytes(StandardCharsets.UTF_8)),
+                StandardCharsets.UTF_8);
+        System.out.println("## decoded:----");
+        System.out.println(decoded);
     }
 }
