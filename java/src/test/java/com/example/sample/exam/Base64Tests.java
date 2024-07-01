290961ca2005a42806d41ef84a9d4902f16ba896
haeram27 <haeram27@gmail.com>
2024-07-02 08:33:45 +0900

    update base64 test

--- java/src/test/java/com/example/sample/exam/Base64Tests.java
+++ java/src/test/java/com/example/sample/exam/Base64Tests.java
@@ -2,15 +2,32 @@ package com.example.sample.exam;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
-
 import org.junit.jupiter.api.Test;
 
 public class Base64Tests {
 
     @Test
+    void base64TempTest() {
+        byte[] originalBytes = new byte[24];
+        for (int i = 0; i < 24; i++) {
+            if (i % 2 == 0) {
+                originalBytes[i] = (byte) 1;
+            }
+        }
+
+        for (var b : originalBytes) {
+            System.out.println(Integer.toBinaryString(Byte.toUnsignedInt(b)));
+        }
+
+        String base64Encoded = Base64.getEncoder().encodeToString(originalBytes);
+        System.out.println(base64Encoded);
+        byte[] base64Decoded = Base64.getDecoder().decode(base64Encoded);
+    }
+
+    @Test
     void base64Test() {
-        byte[] originalBytes = new byte[] { (byte) 255, (byte) 128, (byte) 64, (byte) 32, (byte) 16, (byte) 8, (byte) 4,
-                (byte) 2, (byte) 1 };
+        byte[] originalBytes = new byte[] {(byte) 255, (byte) 128, (byte) 64, (byte) 32, (byte) 16, (byte) 8, (byte) 4,
+                (byte) 2, (byte) 1};
 
         for (var b : originalBytes) {
             System.out.println(Integer.toBinaryString(Byte.toUnsignedInt(b)));
