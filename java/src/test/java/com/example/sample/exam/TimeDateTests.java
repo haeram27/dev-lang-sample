1bf71662d29967867fac7ca245aff73d02fc5065
haeram27 <haeram27@gmail.com>
2024-07-01 23:08:30 +0900

    update time test

--- java/src/test/java/com/example/sample/exam/TimeDateTests.java
+++ java/src/test/java/com/example/sample/exam/TimeDateTests.java
@@ -30,6 +30,33 @@ public class TimeDateTests extends EvaluatedTimeTests {
     }
 
     @Test
+    public void dateTimeFormatTest() {
+        // https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/time/format/DateTimeFormatter.html
+        System.out.println(
+                LocalDateTime.parse("2024-05-13T14:15:16.123456789",
+                        DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+
+        System.out.println(
+                OffsetDateTime.parse("2024-05-13T14:15:16.123456789+09:00",
+                        DateTimeFormatter.ISO_OFFSET_DATE_TIME));
+
+        System.out.println(
+                ZonedDateTime.parse("2024-05-13T14:15:16.123456789+09:00[Asia/Seoul]",
+                        DateTimeFormatter.ISO_ZONED_DATE_TIME));
+
+        String date = "2024-05-13";
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("uuuu-MM-dd");
+
+        System.out.println(OffsetDateTime.of(LocalDate.parse(date, dateFormat),
+                LocalTime.of(20, 30, 40),
+                ZoneOffset.ofHoursMinutes(0, 0)));
+
+        System.out.println(ZonedDateTime.of(LocalDate.parse(date, dateFormat),
+                LocalTime.of(21, 31, 41),
+                ZoneId.of("UTC")));
+    }
+
+    @Test
     public void localDateTimeTest() {
         /*
          * time to long
@@ -54,6 +81,7 @@ public class TimeDateTests extends EvaluatedTimeTests {
         DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("uuuu-MM-dd");
         OffsetDateTime offsetTime = OffsetDateTime.of(LocalDate.parse(date, dateFormat), LocalTime.of(20, 30, 40),
                 ZoneOffset.ofHoursMinutes(0, 0));
+
         System.out.println(offsetTime);
     }
 
@@ -64,7 +92,13 @@ public class TimeDateTests extends EvaluatedTimeTests {
         DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("uuuu-MM-dd");
         ZonedDateTime zoneTime = ZonedDateTime.of(LocalDate.parse(date, dateFormat), LocalTime.of(21, 31, 41),
                 ZoneId.of("UTC"));
-        System.out.println(zoneTime);
-    }
 
+        System.out.println(zoneTime); // 2024-05-13T21:31:41Z[UTC]
+
+        // transform localtime+timezone based on given timezone
+        System.out.println(zoneTime.withZoneSameInstant(ZoneId.of("Asia/Seoul"))); // 2024-05-14T06:31:41+09:00[Asia/Seoul]
+
+        // transform timezone only (localtime x)
+        System.out.println(zoneTime.withZoneSameLocal(ZoneId.of("Asia/Seoul"))); // 2024-05-13T21:31:41+09:00[Asia/Seoul]
+    }
 }
