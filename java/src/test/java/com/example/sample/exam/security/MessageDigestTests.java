61832fc24c784404d0032fdad39f6feed0c628d3
haeram27 <haeram27@gmail.com>
2024-11-04 16:20:52 +0900

    .

--- java/src/test/java/com/example/sample/exam/security/MessageDigestTests.java
+++ java/src/test/java/com/example/sample/exam/security/MessageDigestTests.java
@@ -2,8 +2,10 @@ package com.example.sample.exam.security;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.Security;
 import java.util.Base64;
-
+import org.junit.jupiter.api.Test;
 import lombok.extern.slf4j.Slf4j;
 
 /*
@@ -13,15 +15,41 @@ import lombok.extern.slf4j.Slf4j;
 // Message Digest (SHA-256)
 @Slf4j
 public class MessageDigestTests {
+
+    @Test
     void run() throws NoSuchAlgorithmException {
         String input = "Hello, World!";
         String hash = hash(input);
-        log.info("SHA-256 Hash: " + hash);
+        System.out.println("SHA-256 Hash: " + hash);
+    }
+
+    @Test
+    void printSecurityProviders() {
+        var providers = Security.getProviders();
+        System.out.println(providers.length);
+        if (providers.length > 0) {
+            for (var provider : providers) {
+                System.out.println(provider.getName());
+            }
+        }
     }
 
     public static String hash(String input) throws NoSuchAlgorithmException {
         MessageDigest digest = MessageDigest.getInstance("SHA-256");
         byte[] encodedHash = digest.digest(input.getBytes());
         return Base64.getEncoder().encodeToString(encodedHash);
+    }
+
+    public static String hashWithSalt(String input, byte[] salt) throws NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        digest.update(salt);
+        byte[] encodedHash = digest.digest(input.getBytes());
+        return Base64.getEncoder().encodeToString(encodedHash);
+    }
+    
+    public static byte[] getSecureRandomSalt() {
+        var saltString = Integer.valueOf(new SecureRandom().nextInt()).toString();
+        log.info("salt: " + saltString);
+        return saltString.getBytes();
     }
 }
