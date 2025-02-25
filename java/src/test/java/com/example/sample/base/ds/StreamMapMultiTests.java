f7697ad4c36f4c97473d27063d6f579546868a94
haeram27 <haeram27@gmail.com>
2025-02-25 09:26:20 +0900

    update StreamMapMultiTests

--- java/src/test/java/com/example/sample/base/ds/StreamMapMultiTests.java
+++ java/src/test/java/com/example/sample/base/ds/StreamMapMultiTests.java
@@ -1,8 +1,15 @@
 package com.example.sample.base.ds;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class StreamMapMultiTests {
     /** Methods to make Stream
@@ -38,7 +45,7 @@ public class StreamMapMultiTests {
     }
 
     @Test
-    public void mapMultiListTest() {
+    public void mapMultiListOfListTest() {
 
         List<Integer> firstList = List.of(3, 4);
         List<Integer> secondList = List.of(1, 2);
@@ -58,5 +65,43 @@ public class StreamMapMultiTests {
                     }
                 })
                 .forEach(System.out::println); // 3 4 1 2
+    }
+
+    @Test
+    public void mapMultiListOfMapTest() {
+        // List<Map<String, String>>
+        String jsonString = "["
+                + "{\"ip\": \"1.1.1.1\"},"
+                + "{\"ip\": \"2.2.2.2\"},"
+                + "{\"ip\": \"3.3.3.3\"},"
+                + "{\"ip\": \"4.4.4.4\"}"
+                + "]";
+
+        try {
+            // ObjectMapper
+            ObjectMapper objectMapper = JsonMapper.builder().addModule(new JavaTimeModule()).build();
+
+            // transform JSON to List<Map<String, String>>
+            List<Map<String, String>> listMap = objectMapper.readValue(jsonString, new TypeReference<List<Map<String, String>>>() {});
+
+            // flatten by mapMulti
+            var ipSet = listMap.stream()
+                .<String>mapMulti((map, consumer) -> map.values().forEach(v -> consumer.accept((String) v)))
+                .collect(Collectors.toSet());
+
+            // print set
+            System.out.println(ipSet); // [1.1.1.1, 2.2.2.2, 3.3.3.3, 4.4.4.4]
+
+            // flatten by mapMulti
+            var ipNumberList = listMap.stream()
+                .<String>mapMulti((map, consumer) -> map.values().forEach(v -> consumer.accept((String) v)))
+                .<String>mapMulti((s, consumer) -> Arrays.stream(s.split("\\.")).forEach(e->consumer.accept(e)))
+                .collect(Collectors.toList());
+
+            // print list
+            System.out.println(ipNumberList); // [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4]
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
