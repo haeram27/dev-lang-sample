9f93d97c4666b3b1c97f0f218a2fc6d588252882
haeram27 <haeram27@gmail.com>
2025-03-14 15:59:27 +0900

    update native oss build

--- shell/tools/native-opensource-build/clear.sh
+++ shell/tools/native-opensource-build/clear.sh
@@ -2,5 +2,6 @@
 find . -maxdepth 1 -name openssl-* -type d -exec sudo rm -rf {} \;
 find . -maxdepth 1 -name curl-* -type d -exec sudo rm -rf {} \;
 find . -maxdepth 1 -name mongo-* -type d -exec sudo rm -rf {} \;
+find . -maxdepth 1 -name haproxy-* -type d -exec sudo rm -rf {} \;
 find . -maxdepth 1 -name oss -type d -exec sudo rm -rf {} \;
 sudo rm -f ./oss-*.tgz
