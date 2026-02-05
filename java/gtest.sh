1018d2050f91ed6580fe9c852112a79a2ffee46b
haeram27 <haeram27@gmail.com>
2026-02-05 17:13:04 +0900

    2026-02-05 17:13:04+09:00

--- java/gtest.sh
+++ java/gtest.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 : ${TESTNAME:="HelloTests.hello"}
 [[ -n $1 ]] && TESTNAME=$1
-gradle test --rerun-tasks --tests "${TESTNAME}"
+gradle test ${GRADLE_OPT} --rerun-tasks --tests "${TESTNAME}"
