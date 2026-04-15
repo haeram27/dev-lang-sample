51262f0e7d5fac178cf56db30713c76d35c5d731
haeram27 <haeram27@gmail.com>
2026-04-15 12:58:35 +0900

    2026-04-15 12:58:35+09:00

--- kotlin/gradle.init.kotlin.app.sh
+++ kotlin/gradle.init.kotlin.app.sh
@@ -1,1 +1,6 @@
-gradle init --type kotlin-application --dsl kotlin
+gradle init --type kotlin-application \
+            --dsl kotlin \
+            --package com.example.project \
+#            --project-name my-kotlin-app \  # if not set then use directory name as project-name
+            --no-split-project \
+            --no-incubating
